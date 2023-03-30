adb1ce3e00a48ea23573a3ba696454fa27db393e
msmirnov91 <fourcross@yandex.ru>
2023-03-30 23:15:59 +0300

    move defines to its own file

---
+++ src/hardware_modules/pin_defines.h
@@ -0,0 +1,8 @@
+#define TFT_DC 9
+#define TFT_CS 10
+#define TFT_RST 8
+
+// Uno Hardware SPI
+#define TFT_MISO 12  // NOT CONNECTED
+#define TFT_SDA 11   // MOSI
+#define TFT_CLK 13
--- src/hardware_modules/screen.cpp
+++ src/hardware_modules/screen.cpp
@@ -1,13 +1,5 @@
 #include "screen.h"
-
-#define TFT_DC 9
-#define TFT_CS 10
-#define TFT_RST 8
-
-// Uno Hardware SPI
-#define TFT_MISO 12  // NOT CONNECTED
-#define TFT_SDA 11   // MOSI
-#define TFT_CLK 13
+#include "pin_defines.h"
 
 
 Screen::Screen()
