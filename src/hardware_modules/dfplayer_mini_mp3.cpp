8b12d10ca701ed1d16f95dcea36f4195aeb16bb0
msmirnov91 <fourcross@yandex.ru>
2023-04-10 16:55:41 +0300

    add sound names defines

--- src/hardware_modules/dfplayer_mini_mp3.cpp
+++ src/hardware_modules/dfplayer_mini_mp3.cpp
@@ -1,6 +1,20 @@
 #include "dfplayer_mini_mp3.h"
 #include "pin_defines.h"
 
+#define CLOSE_THE_DOOR_POLITE 1
+#define THANK_YOU 2
+#define CLOSE_THE_DOOR_NERVOUS 3
+#define AT_LEAST 4
+#define CLOSE_THE_DOOR_ANGRY 5
+#define THATS_BETTER 6
+#define HEM_ON 7
+#define EIGHT_THOUSAND_VOLTS 8
+
+#define SOUND_MUTE 0
+#define SOUND_QUIET 10
+#define SOUND_NORMAL 20
+#define SOUND_LOUD 30
+
 
 DFPlayer::DFPlayer()
     : _softwareSerial(
@@ -18,6 +32,6 @@ void DFPlayer::begin()
 		// handle the error
     }
 
-    _player.volume(30);  //Set volume value. From 0 to 30
-    _player.play(1);  //Play the first mp3
+    _player.volume(SOUND_LOUD);  //Set volume value. From 0 to 30
+    _player.play(THANK_YOU);  //Play the first mp3
 }
